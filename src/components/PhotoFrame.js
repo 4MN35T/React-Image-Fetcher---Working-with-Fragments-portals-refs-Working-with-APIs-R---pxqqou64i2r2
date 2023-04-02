8674bb16dc107d5e939816321ec9d12893946b55
4MN35T <tanmaykarmore01@gmail.com>
2023-04-02 14:13:13 +0530

    Update PhotoFrame.js

--- src/components/PhotoFrame.js
+++ src/components/PhotoFrame.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-export const PhotoFrame = () => {
-   return(
-    <>
-    <div className='photoframe'>
-        <img src={props.url}/>
-    </div>
-    </>
-   )
+export const PhotoFrame = (props) => {
+
+    return(
+        <>
+            <div className='photoframe'>
+                <img src={props.url}/>
+                <div className='caption'>{props.title}</div>
+            </div>
+        </>
+    )
 }
